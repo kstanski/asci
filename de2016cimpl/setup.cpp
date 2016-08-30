244ad997d79ecffaee1beee5d03688dcb917d82e
Stanski Kajetan <stanskk1@dana.org.aalto.fi>
2016-08-30 18:24:28 +0300

    Handle different sizes of training dataset

--- de2016cimpl/setup.cpp
+++ de2016cimpl/setup.cpp
@@ -18,33 +18,38 @@ Dataset *setup(const char *filename, int molecules_no, int train_no, int validat
     /* molecules below 5 non-H atoms (lowest energy) all go into training set */
     Molecule *train_mol[train_no];
     Molecule *validate_mol[validate_no];
-    int large_mol_no = molecules_no - BELOW_5_NON_H;
-    Molecule *large_mol_arr[large_mol_no];
-    for (int mol_idx=0; mol_idx<molecules_no; mol_idx++)
+    if (train_no <= BELOW_5_NON_H)
+        stratify(mol_arr, train_mol, train_no, validate_mol, validate_no);
+    else
     {
-        if (mol_idx<BELOW_5_NON_H)
-            train_mol[mol_idx] = mol_arr[mol_idx];
-        else large_mol_arr[mol_idx-BELOW_5_NON_H] = mol_arr[mol_idx];
+        int large_mol_no = molecules_no - BELOW_5_NON_H;
+        Molecule *large_mol_arr[large_mol_no];
+        for (int mol_idx=0; mol_idx<molecules_no; mol_idx++)
+        {
+            if (mol_idx<BELOW_5_NON_H)
+                train_mol[mol_idx] = mol_arr[mol_idx];
+            else large_mol_arr[mol_idx-BELOW_5_NON_H] = mol_arr[mol_idx];
+        }
+
+        int large_train_no = train_no - BELOW_5_NON_H;
+        int large_mol_sample_no = large_train_no + validate_no;
+        Molecule *take_mol[large_mol_sample_no];
+        Molecule **sample_large_mol;
+        if (0<large_mol_sample_no && large_mol_sample_no<large_mol_no)
+        {
+            int leave_no = large_mol_no - large_mol_sample_no;
+            Molecule *leave_mol[leave_no];
+            stratify(large_mol_arr, take_mol, large_mol_sample_no, leave_mol, leave_no);
+            sample_large_mol = take_mol;
+        } else if (large_mol_sample_no == large_mol_no)
+            sample_large_mol = large_mol_arr;
+        else exit(1);
+
+        Molecule *large_train_mol[large_train_no];
+        stratify(sample_large_mol, large_train_mol, large_train_no, validate_mol, validate_no);
+        for (int mol_idx=0; mol_idx<large_train_no; mol_idx++)
+            train_mol[mol_idx+BELOW_5_NON_H] = large_train_mol[mol_idx];
     }
-
-    int large_train_no = train_no - BELOW_5_NON_H;
-    int large_mol_sample_no = large_train_no + validate_no;
-    Molecule *take_mol[large_mol_sample_no];
-    Molecule **sample_large_mol;
-    if (0<large_mol_sample_no && large_mol_sample_no<large_mol_no)
-    {
-        int leave_no = large_mol_no - large_mol_sample_no;
-        Molecule *leave_mol[leave_no];
-        stratify(large_mol_arr, take_mol, large_mol_sample_no, leave_mol, leave_no);
-        sample_large_mol = take_mol;
-    } else if (large_mol_sample_no == large_mol_no)
-        sample_large_mol = large_mol_arr;
-    else exit(1);
-
-    Molecule *large_train_mol[large_train_no];
-    stratify(sample_large_mol, large_train_mol, large_train_no, validate_mol, validate_no);
-    for (int mol_idx=0; mol_idx<large_train_no; mol_idx++)
-        train_mol[mol_idx+BELOW_5_NON_H] = large_train_mol[mol_idx];
 #if VERBOSE
     std::cout << "computing power spectra descriptors" << std::endl;
 #endif // VERBOSE
